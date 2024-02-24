5cc987efd5df3f0e7d7e88b7da6fc98a724d7ade
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-02-24 10:54:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstChar(text) {
   // your code here
+	if(text.length>0)
+	{
+		return text[0];
+	}
+	else
+	{ 
+		return "";
+	}
 }
 
-// Do not change the code below
+// Do not change the code below 
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
